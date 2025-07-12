69371ce69ac5a53abf35c4f6db1300ca9fd916be
Aman Roy <royaman56456@gmail.com>
2025-07-12 16:45:05 +0530

    Frontend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Tailwind is Working!
+      </button>
     </div>
   );
 }
